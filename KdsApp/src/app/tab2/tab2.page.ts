0cbe36ba97b70d76e17d3347df3f79c28aad8382
Felipe Santos <silva93felipe@gmail.com>
2024-08-22 13:42:40 -0300

    feat: Criando o resumo do total de itens da mesa

--- KdsApp/src/app/tab2/tab2.page.ts
+++ KdsApp/src/app/tab2/tab2.page.ts
@@ -13,9 +13,15 @@ import {
     IonNote,
     IonIcon,
     IonFooter,
+    IonActionSheet,
+    IonCard,
+    IonCardHeader,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardTitle,
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { removeOutline, addOutline, trash } from 'ionicons/icons';
+import { removeOutline, addOutline, trash, cart, wallet } from 'ionicons/icons';
 import { Produto } from '../models/produto';
 import { NgFor } from '@angular/common';
 
@@ -38,7 +44,13 @@ import { NgFor } from '@angular/common';
         IonNote,
         IonIcon,
         IonFooter,
-        NgFor
+        NgFor,
+        IonActionSheet,
+        IonCard,
+        IonCardHeader,
+        IonCardContent,
+        IonCardSubtitle, 
+        IonCardTitle, 
     ],
 })
 export class Tab2Page {
@@ -69,25 +81,105 @@ export class Tab2Page {
         },
         {
             id: 5,
+            nome: "Açaí de 500ml",
+            valor: 15,
+            quantidade: 1
+        },
+        {
+            id: 6,
+            nome: "Pastel de frango",
+            valor: 8,
+            quantidade: 1
+        },
+        {
+            id: 7,
+            nome: "Pastel de frango",
+            valor: 8,
+            quantidade: 1
+        },
+        {
+            id: 8,
+            nome: "Pastel de frango",
+            valor: 8,
+            quantidade: 1
+        },
+        {
+            id: 9,
+            nome: "Pastel de frango",
+            valor: 8,
+            quantidade: 1
+        },
+        {
+            id: 10,
+            nome: "Pastel de frango",
+            valor: 8,
+            quantidade: 1
+        },
+        {
+            id: 11,
+            nome: "Pastel de frango",
+            valor: 8,
+            quantidade: 1
+        },
+        {
+            id: 12,
             nome: "Pastel de frango",
             valor: 8,
             quantidade: 1
         }
-    ]
+    ];
+
+    public actionSheetButtons = [
+        {
+          text: 'Adicionar itens',
+          role: 'selected',
+          data: {
+            action: 'delete',
+          },
+        },
+        {
+            text: 'Fechar pedido',
+            role: 'destructive',
+            data: {
+              action: 'delete',
+            },
+          },
+        // {
+        //   text: 'Fechar Pedido',
+        //   role: 'cancel',
+        //   data: {
+        //     action: 'cancel',
+        //   },
+        // },
+    ];
+
     constructor() {
-        addIcons({ removeOutline, addOutline, trash });
+        addIcons({ removeOutline, addOutline, trash, cart, wallet });
     }
 
     public handleQuantidade(action: string, produtoId: number){
         switch (action) {
             case "plus":
-                console.log("somou");
+                ++this.produtos.find(p => p.id == produtoId)!.quantidade;
                 break;
             case "minus":
-                console.log("diminuiu");
+                if(this.produtos.find(p => p.id == produtoId)!.quantidade > 0)
+                    --this.produtos.find(p => p.id == produtoId)!.quantidade;
                 break;
             default:
                 break;
         }
+    }
+
+    public valorTotalItens(){
+        return this.produtos.reduce((acumulador, objeto) => {
+            return acumulador += (objeto.valor * objeto.quantidade);
+        }, 0)
+    }
+
+    public quantidadeTotalItens(){
+        return this.produtos.reduce((acumulador, objeto) => {
+            return acumulador += objeto.quantidade;
+        }, 0)
     }
 }
